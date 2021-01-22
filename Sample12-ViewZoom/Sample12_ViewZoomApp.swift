3669e30293983b975c860e23d3c82b82654069c5
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-22 10:43:34 +0900

    Initial Commit

---
+++ Sample12-ViewZoom/Sample12_ViewZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample12_ViewZoomApp.swift
+//  Sample12-ViewZoom
+//
+//  Created by keiji yamaki on 2021/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Sample12_ViewZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
